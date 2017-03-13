f3be7f66c15c3b5588f8688493a37ff0540e9c82
王丽辉 <lihui.wang@jingli365.com>
2017-03-13 09:47:47 +0800

    直接使用 common/node_ts

--- regTsNode.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by wlh on 2017/3/10.
- */
-'use strict';
-
-let tsConfig = require("./tsconfig.json");
-require("ts-node").register(tsConfig);
--- server.js
+++ server.js
@@ -3,4 +3,4 @@
  */
 'use strict';
 
-require("./regTsNode");
+require("./common/node_ts");

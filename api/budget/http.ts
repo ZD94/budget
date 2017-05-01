3ac054e76a75f37a79961c09b01c762198ee87fe
wanglihui <lihui.wang@tulingdao.com>
2017-05-01 19:57:19 +0800

    http请求单独提取到http.ts

---
+++ api/budget/http.ts
@@ -0,0 +1,37 @@
+/**
+ * Created by wlh on 2017/4/28.
+ */
+
+'use strict';
+import {IQueryBudgetParams} from "../../_types/budget";
+import ApiTravelBudget from "./index";
+
+export = function(app) {
+    app.post('/api/v1/budget/make', (req, res, next) => {
+        let qs: IQueryBudgetParams
+        let json = req.body.json;
+        if (typeof json == 'string') {
+            qs = JSON.parse(json);
+        } else {
+            qs = json;
+        }
+        return ApiTravelBudget.createBudget(qs)
+            .then( (result) => {
+                res.json(result);
+            })
+            .catch(next);
+    })
+
+    app.get('/api/v1/budget/info', (req, res, next) => {
+        let {appid, id} = req.query;
+        return ApiTravelBudget.getBudgetCache({appid, id})
+            .then( (result) => {
+                res.json(result);
+            })
+            .catch(next);
+    })
+
+    app.use('/api/v1/budget', function(err, req, res, next) {
+        res.json(err);
+    })
+}

f0e72404d168c7e59a3d5ce8244f60dcd105d7e3
wanglihui <lihui.wang@tulingdao.com>
2017-06-05 18:45:29 +0800

    fixed 火车打分两次问题

--- api/budget/prefer/price.ts
+++ api/budget/prefer/price.ts
@@ -86,6 +86,9 @@ class PricePrefer extends AbstractPrefer<(IFinalHotel|IFinalTicket)> {
         }
 
         data = data.map( (v: (IFinalHotel|IFinalTicket)) => {
+            if (v['type'] == ETrafficType.TRAIN) {
+                return v;
+            }
             if(!v.reasons) v.reasons = [];
             if(!v.score) v.score = 0;
             if (self.level.indexOf(parseInt(v['cabin'])) < 0 && self.level.indexOf(parseInt(v['star'])) < 0) {

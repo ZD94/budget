10fcbafff2bb4e0185b143ba1cd19c62d2fd53a6
wanglihui <lihui.wang@tulingdao.com>
2017-06-09 18:31:53 +0800

    修改获取数据逻辑

--- api/hotels/index.ts
+++ api/hotels/index.ts
@@ -7,14 +7,29 @@
 import API from "@jingli/dnode-api";
 import {AbstractDataSupport} from "../data-support";
 import {TASK_NAME} from "../types";
+import {IHotel} from "../../_types/budget";
 
-export interface IHotel {}
+export interface ISearchHotelParams {
+    checkInDate: string;
+    checkOutDate: string;
+    city: string;
+    latitude?: number;
+    longitude?: number;
+}
+
 export class HotelSupport extends AbstractDataSupport<IHotel> {
 
-    async search_hotels(params) {
-        let hotels = await this.getData(TASK_NAME.HOTEL, params);
-        let abroadHotels = await this.getData(TASK_NAME.HOTEL_ABROAD, params);
-        return [...hotels, ...abroadHotels];
+    async search_hotels(params: ISearchHotelParams) {
+        let {city, latitude, longitude} = params;
+        let cityObj = await API['place'].getCityInfo({cityCode: city});
+        if (!latitude || !longitude) {
+            params.latitude = cityObj.latitude;
+            params.longitude = cityObj.longitude;
+        }
+        if (!cityObj.isAbroad) {
+            return this.getData(TASK_NAME.HOTEL, params);
+        }
+        return this.getData(TASK_NAME.HOTEL_ABROAD, params);
     }
 }
 

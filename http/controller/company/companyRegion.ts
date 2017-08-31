f2711c48e449981c48b0eec952a2c80ad44bd59e
俞长龙 <changlong.yu@jingli365.com>
2017-08-31 15:35:17 +0800

    Update companyRegion.ts

--- http/controller/company/companyRegion.ts
+++ http/controller/company/companyRegion.ts
@@ -147,7 +147,7 @@ export class CompanyRegionController extends AbstractController {
         }
         let obj = await Models.companyRegion.get(id);
         let isDeleted = await obj.destroy();
-        res.reply(0, isDeleted);
+        res.json(this.reply(0, isDeleted));
     }
 
 

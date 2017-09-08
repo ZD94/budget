d626483802067ebe813c1c5d7a93628acdf50568
hexisen <616082046@qq.com>
2017-09-08 11:15:09 +0800

    增加指定域名跨越访问

--- http/index.ts
+++ http/index.ts
@@ -11,11 +11,37 @@ import {scannerControllers, registerControllerToRouter} from "@jingli/restful";
 import path = require("path");
 import express = require("express");
 import {authenticate} from "./auth";
+
 var router = express.Router();
 
 scannerControllers(path.join(__dirname, 'controller'));
 registerControllerToRouter(router);
 
+let allowOrigin = [
+    "localhost:4002",
+    "jingli365"
+];
+
+function checkOrigin( origin ){
+    for(let item of allowOrigin){
+        if(origin.indexOf(item) > -1){
+            return true;
+        }
+    }
+
+    return false;
+}
+
 export async function initHttp(app) {
+    
+    app.use((req, res, next)=>{
+        if(checkOrigin(req.headers.origin)){
+            res.header('Access-Control-Allow-Origin', req.headers.origin);
+        }
+        res.header('Access-Control-Allow-Methods', '*');
+        res.header('Access-Control-Allow-Headers', '*');
+        next();
+    });
+
     app.use('/api/v1', authenticate, router);
 }

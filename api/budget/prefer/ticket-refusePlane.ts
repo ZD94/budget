56789449712b536d7b01e5509e511f8a8603e520
wangyali <yali.wang@jingli365.com>
2017-10-16 18:40:58 +0800

    不乘坐飞机的prefer

---
+++ api/budget/prefer/ticket-refusePlane.ts
@@ -0,0 +1,31 @@
+'use strict';
+import {IFinalTicket, ETrafficType} from "_types/budget";
+import {AbstractPrefer} from "./index";
+
+class RefusedPlanePrefer extends AbstractPrefer<IFinalTicket> {
+    private score: number;
+
+    constructor(public name: string, options: any) {
+        super(name, options);
+        if (!this.score) {
+            this.score = 0;
+        }
+    }
+    async markScoreProcess(tickets: IFinalTicket[]) : Promise<IFinalTicket[]> {
+        let self = this;
+
+        tickets = tickets.map( (v) => {
+            if (!v['score']) v['score'] = 0;
+            if (!v.reasons) v['reasons'] = [];
+            if (v.type == ETrafficType.PLANE) {
+                v['score'] += self.score;
+                v.reasons.push(`不乘坐飞机 ${self.score}`);
+                return v;
+            }
+            return v;
+        })
+        return tickets;
+    }
+}
+
+export= RefusedPlanePrefer;

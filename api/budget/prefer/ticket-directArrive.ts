236e1ecf4586c744ff7351a483fd07feeb0385fb
俞长龙 <changlong.yu@jingli365.com>
2017-10-17 14:22:01 +0800

    Update ticket-directArrive.ts

--- api/budget/prefer/ticket-directArrive.ts
+++ api/budget/prefer/ticket-directArrive.ts
@@ -27,11 +27,12 @@ class DirectArrivePrefer extends AbstractPrefer<IFinalTicket> {
             if (!v.reasons) {
                 v.reasons = [];
             }
+            
             if (v.segs && v.segs.length) {
                 let l = v.segs.length;
                 if (l > 1) {
                     let score = self.baseScore * ( 1- (l - 1) * self.rate);
-                    v.score += Math.round(score);
+                    v.score += score;
                     v.reasons.push(`需经过${l-1}次中转: ${score}`);
                 }
             }

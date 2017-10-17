8aa3128976feb61d46d10108c0bec7136747fc7c
wangyali <yali.wang@jingli365.com>
2017-10-17 11:06:56 +0800

    添加logo

--- api/supplier/sql/update.1710131520.ts
+++ api/supplier/sql/update.1710131520.ts
@@ -1,24 +1,25 @@
 let uid = require("uuid");
 var commonSuppliers = [
-    {name:"飞猪", logo: 'logo/feizhu.png', enName: '', alias: '飞猪'},
+    {name:"飞猪", logo: 'logo/piggy.png', enName: 'piggy', alias: '飞猪'},
     {name:"去哪儿网", logo: 'logo/qunar.png', enName: 'qunar', alias: '去哪儿'},
-    {name:"携程旅行网", logo: 'logo/xiecheng.png', enName: 'ctrip', alias: '携程'},
-    {name:"同程旅游", logo: 'logo/tongcheng.png', enName: '', alias: '同程'},
-    {name:"KIWI.COM", logo: 'logo/kiwi.png', enName: 'kiwi.com', alias: 'KIWI'},
-    {name:"非凡旅行", logo: 'logo/feifan.png', enName: '', alias: '非凡旅行'},
-    {name:"欧美嘉旅行网", logo: 'logo/oumeijia.png', enName: '', alias: '欧美嘉'},
-    {name:"途牛旅游网", logo: 'logo/tuniu.png', enName: '', alias: '途牛'},
-    {name:"Chuangchen Air", logo: 'logo/Chuangchen.png', enName: '', alias: 'Chuangchen Air'},
-    {name:"ebookers", logo: 'logo/ebookers.png', enName: '', alias: 'ebookers'},
-    {name:"GotoGate", logo: 'logo/gotogate.png', enName: '', alias: 'GotoGate'},
-    {name:"Mytrip", logo: 'logo/mytrip.png', enName: '', alias: 'Mytrip'},
-    {name:"Travel2Be", logo: 'logo/travel2be.png', enName: '', alias: 'Travel2Be'},
-    {name:"Travelgenio", logo: 'logo/travelgenio.png', enName: '', alias: 'Travelgenio'},
-    {name:"Travelliker爱遊人", logo: 'logo/travelliker.png', enName: '', alias: 'Travelliker'},
-    {name:"Tripsta", logo: 'logo/tripsta.png', enName: '', alias: 'Tripsta'},
-    {name:"ebookers", logo: 'logo/ebookers.png', enName: '', alias: 'ebookers'},
-    {name:"Tripair", logo: 'logo/tripair.png', enName: '', alias: 'Tripair'},
-    {name:"ZUJI", logo: 'logo/zuji.png', enName: '', alias: 'ZUJI'}
+    {name:"携程旅行网", logo: 'logo/ctrip.png', enName: 'ctrip', alias: '携程'},
+    {name:"同程旅游", logo: 'logo/tongcheng.png', enName: 'tongcheng', alias: '同程'},
+    {name:"KIWI.COM", logo: 'logo/kiwi.png', enName: 'kiwi', alias: 'KIWI'},
+    {name:"非凡旅行", logo: 'logo/feifan.png', enName: 'feifan', alias: '非凡旅行'},
+    {name:"欧美嘉旅行网", logo: 'logo/omega.png', enName: 'omega', alias: '欧美嘉'},
+    {name:"途牛旅游网", logo: 'logo/tuniu.png', enName: 'tuniu', alias: '途牛'},
+    {name:"Chuangchen Air", logo: 'logo/chenguanglvxing.png', enName: 'chuangchen', alias: 'Chuangchen Air'},
+    {name:"GotoGate", logo: 'logo/gotogate.png', enName: 'gotogate', alias: 'GotoGate'},
+    {name:"ebookers", logo: 'logo/ebookers.png', enName: 'ebookers', alias: 'ebookers'},
+    {name:"Mytrip", logo: 'logo/mytrip.png', enName: 'mytrip', alias: 'Mytrip'},
+    {name:"Travel2Be", logo: 'logo/travel2be.png', enName: 'travel2be', alias: 'Travel2Be'},
+    {name:"Travelgenio", logo: 'logo/travelgenio.png', enName: 'travelgenio', alias: 'Travelgenio'},
+    {name:"Travelliker爱遊人", logo: 'logo/travelliker.png', enName: 'travelliker', alias: 'Travelliker'},
+    {name:"Tripsta", logo: 'logo/tripsta.png', enName: 'tripsta', alias: 'Tripsta'},
+    {name:"ebookers", logo: 'logo/ebookers.png', enName: 'ebookers', alias: 'ebookers'},
+    {name:"Tripair", logo: 'logo/tripair.png', enName: 'tripair', alias: 'Tripair'},
+    {name:"ZUJI", logo: 'logo/zuji.png', enName: 'zuji', alias: 'ZUJI'},
+    {name:"bravofly", logo: 'logo/bravofly.png', enName: 'bravofly', alias: 'bravofly'}
 ];
 
 

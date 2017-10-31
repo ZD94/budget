3a3fd6f02cceaa581b1106d34436dedf53af7d58
vritser <1429594204@qq.com>
2017-10-31 14:41:22 +0800

    use place api instead common-api

--- http/controller/city.ts
+++ http/controller/city.ts
@@ -14,28 +14,77 @@ export class CityController extends AbstractController {
     }
 
     $isValidId(id: string) {
-        return /^CTW?_\d+$/.test(id);
+        return /^\d+$/.test(id);
     }
 
     async get(req, res, next) {
         let {id} = req.params;
-        let city = await API['place'].getCityInfo({cityCode: id});
-        city = this.transform(city);
-        res.json(this.reply(0, city));
+        const resp: any = await restfulAPIUtil.proxyHttp({
+            url: `/city/${id}`,
+            method: 'GET'
+        })
+
+        if(resp.code === 0) {
+            return res.send(this.reply(0, this.transform2(resp.data)))
+        }
+        return res.send(resp.code, null);
     }
 
     async find(req, res, next) {
         let {keyword} = req.query;
-        let cities = [];
-        if (!keyword) {
-            cities = await API['place'].queryHotCity({limit: 20});
-        } else {
-            cities = await API['place'].queryCity({keyword: keyword});
+        const resp: any = keyword
+            ? await restfulAPIUtil.proxyHttp({url: `/city/search/`, method: 'GET'})
+            : await restfulAPIUtil.proxyHttp({url: `/city`, method: 'GET'})
+        const result = resp.code === 0
+            ? this.reply(0, resp.data.map(this.transform2))
+            : this.reply(resp.code, null);
+        return res.send(result);
+    }
+
+    @Router('/nearby/:longitude/:latitude', 'get')
+    async findNearCity(req, res, next) {
+        let {latitude, longitude} = req.params,
+            pattern = /^\d+\.?\d+$/;
+            
+        const isValid = pattern.test(latitude) && pattern.test(longitude);
+        if(!isValid){
+            return res.send(this.reply(400, null));
         }
-        cities = cities.map( (city) => {
-            return this.transform(city);
+        const resp: any = await restfulAPIUtil.proxyHttp({
+            url: `/city/nearby/${longitude},${latitude}`,
+            method: 'GET'
         });
-        res.json(this.reply(0, cities));
+
+        return res.send(this.processResp(resp));
+    }
+
+    @Router('/:id/children', 'get')
+    async getChildren(req, res, next) {
+        let {id} = req.params;
+        const resp: any = await restfulAPIUtil.proxyHttp({
+            url: `/city/${id}/children`,
+            method: 'GET'
+        });
+
+        return res.send(this.processResp(resp));
+    }
+
+    private processResp(resp) {
+        return resp.code === 0
+            ? this.reply(0, resp.data.map(this.transform2))
+            : this.reply(resp.code, null);
+    }
+
+    private transform2(city) {
+        return {
+            id: city.id,
+            name: city.name,
+            pinyin: city.pinyin,
+            letter: city.letter,
+            latitude: city.lat,
+            longitude: city.lng,
+            parentId: city.parentId,
+        }
     }
 
     private transform(city) {

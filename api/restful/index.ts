15de361954958003b709b4173fa5bfc4b660a711
vritser <1429594204@qq.com>
2017-10-31 11:51:11 +0800

    add restful util

---
+++ api/restful/index.ts
@@ -0,0 +1,42 @@
+/**
+ * Created by lsw on 31/10/2017.
+ */
+
+var request = require("request");
+var Config = require("@jingli/config");
+
+export class RestfulAPIUtil {
+
+    async proxyHttp(params:{
+        url:string;
+        body?:object;
+        method:string;
+        qs?:object;
+    }){
+        let {url, body={}, method="get", qs={}} = params;
+        return new Promise((resolve, reject) => {
+            request({
+                uri: Config.cloudAPI + url,
+                body,
+                json: true,
+                method,
+                qs,
+                headers: {
+                    key: Config.cloudKey
+                }
+            }, (err, resp, result) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                if (typeof result == 'string') {
+                    result = JSON.parse(result);
+                }
+                return resolve(result);
+            });
+        })
+    }
+}
+
+
+export let restfulAPIUtil = new RestfulAPIUtil();

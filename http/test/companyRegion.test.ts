f02371c1eafaac3eaeee657a15887f80e4a33f65
lisongwei <songwei.li@jingli365.com>
2017-11-02 19:06:35 +0800

    add test

--- http/test/companyRegion.test.ts
+++ http/test/companyRegion.test.ts
@@ -2,7 +2,34 @@
  * Created by wlh on 2017/11/2.
  */
 
-import request from 'supertest';
-import assert from 'assert';
-
+const request = require('supertest');
+const assert = require('assert');
+import {getFullPath, getToken} from "./helper";
  
+describe('/companyRegion', () => {
+    const regions = [];
+    const url = getFullPath('/companyRegion');
+    it('/', done => {
+        request(url)
+            .get('/')
+            .set('token', getToken())
+            .expect(200)
+            .end((err, resp) => {
+                if(err) return done(err);
+                regions.push([...resp.body.data]);
+                done(assert.equal(resp.body.code, 0));
+            });
+    });
+    it('/:id', done => {
+        request(url)
+            .get(`/${regions[0].id}`)
+            .set('token', getToken())
+            .expect(200)
+            .end((err, resp) => {
+                if(err) return done(err);
+                assert.equal(resp.body.code, 0);
+                assert.equal(typeof resp.body.data, 'object');
+                done();
+            })
+    });
+})

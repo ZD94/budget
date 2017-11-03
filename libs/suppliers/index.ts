908f3dcf41ebb43d89cced4255e093b6b9b17845
李泽霖 <zelin.li@jingli365.com>
2017-11-03 11:54:13 +0800

    预订跳转

---
+++ libs/suppliers/index.ts
@@ -0,0 +1,49 @@
+
+import { WebRobot } from '../web_robot/index';
+import { SupplierOrder, ReserveLink } from './interface';
+export { SupplierOrder, ReserveLink } from './interface';
+
+export abstract class SupplierWebRobot extends WebRobot{
+    constructor(origin: string){
+        super(origin);
+    }
+
+    abstract login(authData: any): Promise<any>;
+    abstract getOrderList(): Promise<SupplierOrder[]>;
+    
+    getBookLink(options): Promise<ReserveLink>{
+        return Promise.resolve(null);
+    }
+}
+
+interface ReserveLinkObject{
+    url: string
+}
+
+interface SupplierWebRobotConstructor{
+    new(): SupplierWebRobot
+}
+
+let suppliers: {
+    [key: string]: SupplierWebRobotConstructor
+};
+
+function initSuppliers(){
+    suppliers = {
+        ctrip_com:       require('./ctrip_com'),
+        qunar_com_m:     require('./qunar_com'),
+        taobao_com :     require('./taobao_com'),
+        ly_com :         require('./ly_com'),
+        jingzhong_com:   require('./jingzhong_com'),
+        kiwi_com:        require('./kiwi_com')
+    }
+}
+
+export function getSupplier(id): SupplierWebRobot {
+    if(!suppliers){
+        initSuppliers();
+    }
+    return new suppliers[id]();
+}
+
+export type SupplierGetter = typeof getSupplier;
---
+++ libs/suppliers/interface.ts
@@ -0,0 +1,21 @@
+
+import { EPayType, EInvoiceFeeTypes } from '_types/tripPlan/index';
+
+export interface SupplierOrder{
+    id: string;
+    price: number;
+    date: Date;
+    persons: string[];
+    desc: string;
+    orderType: EInvoiceFeeTypes;
+    parType: EPayType;
+    number?: string;
+    starCityName?: string;
+    endCityName?: string;
+}
+
+export interface ReserveLink{
+    url: string;
+    jsCode: string;
+    indexUrl?: string;
+}

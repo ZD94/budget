8ead1d25098b56cc413417358ffd95c72fee5254
yuchanglong <changlong.yu@jingli365.com>
2017-11-05 19:30:08 +0800

    fix restful test data

--- api/company/sql/init.sql
+++ api/company/sql/init.sql
@@ -1,2 +1,8 @@
 --schema=company
-insert into company.companies (id, name, is_Upload_Invoice, is_Open_Subsidy_Budget, type, app_Id, app_Secret, created_at, updated_at) values ('1b5dc6fc-ea3d-411d-b747-3a301abb40ab', '鲸力智享', true, true, 3, '756b12b3-e243-41ae-982f-dbdfb7ea7e92', '6c8f2cfd-7aa4-48c7-9d5e-913896acec12', now(), now())
+insert into company.companies (id, name, is_Upload_Invoice, is_Open_Subsidy_Budget, type, app_Id, app_Secret, created_at, updated_at)
+  values ('1b5dc6fc-ea3d-411d-b747-3a301abb40ab', '鲸力智享', true, true, 3, '756b12b3-e243-41ae-982f-dbdfb7ea7e92', '6c8f2cfd-7aa4-48c7-9d5e-913896acec12', now(), now());
+
+
+insert into company.companies (id, name, is_Upload_Invoice, is_Open_Subsidy_Budget, type, app_Id, app_Secret, created_at, updated_at)
+  values ('60294e10-1448-11e7-aa89-6b4a98eecf40', '笑傲江湖', true, true, 3, '756b12b3-e243-41ae-982f-dbdfb7ea7e92', '6c8f2cfd-7aa4-48c7-9d5e-913896acec12', now(), now());
+

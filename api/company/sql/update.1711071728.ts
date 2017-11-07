95c76841e1b625000020fd9596fa096e920d4edf
lisongwei <songwei.li@jingli365.com>
2017-11-07 18:20:43 +0800

    modify update.1711071728.ts

--- api/company/sql/update.1711071728.ts
+++ api/company/sql/update.1711071728.ts
@@ -6,20 +6,20 @@
 import Sequelize = require('sequelize');
 import config = require('@jingli/config');
 import md5 = require('md5');
+const uuid = require('uuid');
 
-export = async function(db, transition) {
-    // let db2 = new Sequelize(config.oldPostgres.url);
+export = async function (db, transition) {
+
     let db2 = new Sequelize(config.oldPostgres.url);
 
-    const sql2 = `select * from company.companies where deleted_at is null`;
+    const sql2 = `select distinct a.mobile,a.pwd, s.company_id from auth.accounts a 
+        join staff.staffs s on s.account_id = a.id
+        where a.deleted_at is null and a.mobile is not null`;
+    const usrs = await db2.query(sql2);
 
-    let companies = await db2.query(sql2);
-
-    for (let c of companies[0]) {
-        const appId = md5(c.id);
-        let sql = `insert into company.companies(id,name,created_at,updated_at,type,app_id,app_secret) 
-            values(${c.id},${c.name},${c.created_at},${c.updated_at},1,${appId},${appId.slice(-8)})`;
-
-        await db.query(sql);
+    for (let u of usrs) {
+        let sql = `insert into auth.accounts(id,mobile,pwd,created_at,updated_at,company_id) 
+            values(${uuid.v1()},${u.mobile},${u.pwd},now(),now(),${u.company_id})`
+        await db.query(sql)
     }
 }

d11a1586f822343129cf6ac844f0d1503a1289af
wanglihui <lihui.wang@tulingdao.com>
2017-11-08 21:28:02 +0800

    fixed company create

--- api/company/index.ts
+++ api/company/index.ts
@@ -3,4 +3,45 @@
  */
 'use strict';
 
-export = {};
+import L from "@jingli/language";
+import { Models } from '_types';
+import { Company } from '_types/company';
+import md5 = require("md5");
+const companyCols = Company['$fieldnames'];
+import { CompanyType } from 'api/auth';
+
+export interface CreateCompanyParams {
+    id: string;
+    name: string;
+    priceLimitType?: number;
+    appointedPubilcSuppliers: any;
+}
+
+export default class CompanyModule {
+
+    public async create(params: CreateCompanyParams) {
+        let { id } = params;
+        if (!id || typeof (id) == 'undefined') {
+            throw new L.ERROR_CODE_C(500, '缺少必要参数');
+        }
+        let checkCompany = await Models.company.get(id);
+        if (checkCompany) {
+            throw new L.ERROR_CODE_C(403, '企业已经存在');
+        }
+
+        let company = Company.create({
+            id
+        });
+        for (let key in params) {
+            if (companyCols.indexOf(key) >= 0) {
+                company[key] = params[key];
+            }
+        }
+        const appId = md5(id);
+        company.type = CompanyType.GENERAL;
+        company.appId = appId;
+        company.appSecret = appId.slice(-8);
+        company = await company.save();
+        return company;
+    }
+}

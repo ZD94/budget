26582f7a2701c8ecdea692c851bb02feeebfdb73
hexisen <616082046@qq.com>
2017-12-29 12:22:03 +0800

    update package.json

--- model/budget/index.ts
+++ model/budget/index.ts
@@ -2,7 +2,7 @@
  * @Author: Mr.He 
  * @Date: 2017-12-20 18:56:43 
  * @Last Modified by: Mr.He
- * @Last Modified time: 2017-12-28 10:42:05
+ * @Last Modified time: 2017-12-29 17:50:26
  * @content what is the content of this file. */
 
 export * from "./interface";
@@ -98,13 +98,11 @@ export class Budget {
 
         if (budgetOrder.step != STEP.FINAL) {
             //10s 后拉取最终预算
-            setTimeout(() => {
-                this.getFinalBudget(budgetOrder);
-            }, 1000 * 5);
+            this.getFinalBudget(budgetOrder);
         }
 
         finallyResult.step = budgetOrder.step;
-        console.log('**********', budgetOrder);
+        // console.log('**********', JSON.stringify(finallyResult));
         console.log("using time : ", Date.now() - times);
         return finallyResult;
     }
@@ -141,7 +139,7 @@ export class Budget {
             if (item.step != STEP.FINAL) {
                 num++;
                 if (num < 4) {
-                    console.error("data-store 返回的数据不是全 FIN; 尝试重新拉取 第",num,"次");
+                    console.error("data-store 返回的数据不是全 FIN; 尝试重新拉取 第", num, "次");
                     return await this.getFinalBudget(budgetOrder, num);
                 }
             }
@@ -171,7 +169,7 @@ export class Budget {
                 uri: callbackUrl,
                 method: "post",
                 body: result,
-                headers:{
+                headers: {
                     appid: config.agent.appId,
                     sign: sign
                 },
@@ -199,53 +197,43 @@ export let budget = new Budget();
 
 
 
-// setTimeout(async () => {
-//     console.log("go go");
-//     await budget.getBudget({
-//         callbackUrl: "12344",
-//         "travelPolicyId": "ae6e7050-af2a-11e7-abf6-9f811e5a6ff9",
-//         "companyId": "e3e7e690-1b7c-11e7-a571-7fedc950bceb",
-//         "staffs": [
-//             {
-//                 "gender": 1,
-//                 "policy": "domestic"
-//             }
-//         ],
-//         originPlace: 'CT_131',
-//         goBackPlace: 'CT_131',
-//         isRoundTrip: false,
-//         destinationPlacesInfo:
-//             [{
-//                 destinationPlace: 'CT_075',
-//                 leaveDate: "2018-01-26T10:00:00.000Z",
-//                 goBackDate: "2018-01-27T01:00:00.000Z",
-//                 latestArrivalDateTime: "2018-01-26T10:00:00.000Z",
-//                 earliestGoBackDateTime: "2018-01-27T01:00:00.000Z",
-//                 isNeedTraffic: true,
-//                 isNeedHotel: true,
-//                 reason: ''
-//             },
-//                 // {
-//                 //     destinationPlace: 'CT_289',
-//                 //     leaveDate: "2018-01-27T10:00:00.000Z",
-//                 //     goBackDate: "2018-01-28T01:00:00.000Z",
-//                 //     latestArrivalDateTime: "2018-01-27T10:00:00.000Z",
-//                 //     earliestGoBackDateTime: "2018-01-28T01:00:00.000Z",
-//                 //     isNeedTraffic: true,
-//                 //     isNeedHotel: true,
-//                 //     reason: ''
-//                 // },
-//                 // {
-//                 //     destinationPlace: 'CT_179',
-//                 //     leaveDate: "2018-01-28T10:00:00.000Z",
-//                 //     goBackDate: "2018-01-29T01:00:00.000Z",
-//                 //     latestArrivalDateTime: "2018-01-28T10:00:00.000Z",
-//                 //     earliestGoBackDateTime: "2018-01-29T01:00:00.000Z",
-//                 //     isNeedTraffic: true,
-//                 //     isNeedHotel: true,
-//                 //     reason: ''
-//                 // }
-//             ],
-//     })
+setTimeout(async () => {
+    console.log("go go");
+    await budget.getBudget({
+        "callbackUrl": "12344",
+        "travelPolicyId": "ae6e7050-af2a-11e7-abf6-9f811e5a6ff9",
+        "companyId": "e3e7e690-1b7c-11e7-a571-7fedc950bceb",
+        "staffs": [
+            {
+                "gender": 1,
+                "policy": "domestic"
+            }
+        ],
+        "originPlace": "CT_131",
+        "goBackPlace": "CT_131",
+        "isRoundTrip": false,
+        "destinationPlacesInfo":
+            [{
+                "destinationPlace": "CT_075",
+                "leaveDate": "2018-01-26T10:00:00.000Z",
+                "goBackDate": "2018-01-27T01:00:00.000Z",
+                "latestArrivalDateTime": "2018-01-26T10:00:00.000Z",
+                "earliestGoBackDateTime": "2018-01-27T01:00:00.000Z",
+                "isNeedTraffic": true,
+                "isNeedHotel": true,
+                "reason": ""
+            },
+            {
+                "destinationPlace": "CT_289",
+                "leaveDate": "2018-01-27T10:00:00.000Z",
+                "goBackDate": "2018-01-28T01:00:00.000Z",
+                "latestArrivalDateTime": "2018-01-27T10:00:00.000Z",
+                "earliestGoBackDateTime": "2018-01-28T01:00:00.000Z",
+                "isNeedTraffic": true,
+                "isNeedHotel": true,
+                "reason": ""
+            }
+            ]
+    })
 
-// }, 8000);
+}, 8000);

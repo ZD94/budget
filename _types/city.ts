e8e03f599493f65859a2f228512255ea2066a30f
wangyali <yali.wang@jingli365.com>
2018-01-10 11:23:32 +0800

    地点信息向上查找逻辑

--- _types/city.ts
+++ _types/city.ts
@@ -2,9 +2,13 @@
  * Created by wlh on 2017/3/13.
  */
 
+
 'use strict';
 const API = require("@jingli/dnode-api");
 import LRU = require("lru-cache");
+import L from '@jingli/language';
+import { restfulAPIUtil } from 'api/restful';
+import {CoordDispose, Degree} from "../libs/place/placeUtil";
 var cache = LRU(50);
 
 export interface ICity {
@@ -16,8 +20,11 @@ export interface ICity {
     longitude: number;
     latitude: number;
     code?: string;  //三字码
+    parentId: string;
+    pinyin: string;
+    countryCode: string;
+    fcode: string;
 }
-
 
 export class CityService {
 
@@ -26,10 +33,98 @@ export class CityService {
         if  (city) {
             return city;
         }
-        city = await API.place.getCityInfo({cityCode: id});
+
+        // city = await API.place.getCityInfo({cityCode: id});
+
+        let result: any = await restfulAPIUtil.proxyHttp({
+            uri: `/city/${id}`,
+            method: 'GET'
+        });
+        if(result.code == 0){
+            city = result.data;
+        }
         if (city) {
             cache.set(id, city);
         }
         return city;
     }
+
+    static async findCities(options) :Promise<any> {
+        let result: any = await restfulAPIUtil.proxyHttp({ uri: `/city`, method: 'GET', qs: options });
+        console.info("result===llllldddd", result);
+        if(result.code == 0){
+            return result.data;
+        }
+        return null;
+    }
+
+    /**
+     * 根据城市id向上获取有火车站或机场的城市信息
+     * @param params
+     * @returns {Promise<any | any | any>}
+     */
+    static async getSuperiorCityInfo(params) :Promise<ICity>  {
+        let self = this;
+        let cityId = params.cityId;
+        if (!cityId) {
+            throw L.ERR.ERROR_CODE_C(500, "城市不存在");
+        }
+        let city = await this.getCity(cityId);
+
+        if (!city) {
+            throw L.ERR.ERROR_CODE_C(500, "城市不存在");
+        }
+        if(city.fcode == "AIRP" || city.fcode == "RSTN"){
+            let parentCity = await this.getCity(city.parentId);
+            return parentCity;
+        }
+
+        let child_cities = await this.findCities({
+            where: {parentId: city.id, fcode: ["AIRP", "RSTN"]}});
+
+        if(child_cities && child_cities.length){
+            return city;
+        }
+
+        let deg = new Degree(parseFloat(city.latitude+""), parseFloat(city.longitude+""));
+        let result = CoordDispose.GetDegreeCoordinates(deg, 100);
+        let nearbyStations: any[] = await this.findCities({
+            where: {
+                lat: {
+                    $gte: result["lat_min"],
+                    $lte: result["lat_max"]
+                },
+                lng: {
+                    $gte: result["lng_min"],
+                    $lte: result["lng_max"]
+                },
+                fcode: ["AIRP", "RSTN"]
+            }});
+
+
+        nearbyStations = nearbyStations.filter((item: any)=>{
+            return item.countryCode == city.countryCode;
+        })
+
+        if(nearbyStations && nearbyStations.length){
+            orderByDistance(deg, nearbyStations);
+            let parentCity = await this.getCity(nearbyStations[0].parentId);
+            return parentCity;
+        }
+
+        let returnResult = await self.getSuperiorCityInfo({cityId: city.parentId});
+        return returnResult;
+    }
+
+}
+
+function orderByDistance(deg: Degree, nearbyStations: any){
+    nearbyStations.forEach((item) => {
+        let degItem = new Degree(parseFloat(item.latitude+""), parseFloat(item.longitude+""));
+        let distance = CoordDispose.GetDistanceGoogle(deg, degItem);
+        item.distance = distance;
+    })
+    nearbyStations.sort( (item1, item2) => {
+        return item1.distance - item2.distance;
+    });
 }
---
+++ libs/place/placeUtil.ts
@@ -0,0 +1,65 @@
+// 经纬度坐标
+export class Degree {
+    public X: number;
+    public Y: number;
+    constructor(lat: number, lng: number) {
+        this.X = lat;
+        this.Y = lng;
+    }
+}
+export var EARTH_RADIUS = 6378137.0;//地球半径(米)
+export class CoordDispose {
+    // 角度数转换为弧度公式
+    private static  radians(d) {
+        return d * Math.PI / 180.0;
+    }
+
+    // 弧度转换为角度数公式
+    private static degrees(d) {
+        return d * (180 / Math.PI);
+    }
+    // 计算两个经纬度之间的直接距离
+    public static GetDistance(Degree1, Degree2) {
+        var radLat1 = this.radians(Degree1.X);
+        var radLat2 = this.radians(Degree2.X);
+        var a = radLat1 - radLat2;
+        var b = this.radians(Degree1.Y) - this.radians(Degree2.Y);
+        var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
+        Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s;
+    }
+    // 计算两个经纬度之间的直接距离(google 算法)
+    public static GetDistanceGoogle(Degree1, Degree2) {
+        var radLat1 = this.radians(Degree1.X);
+        var radLng1 = this.radians(Degree1.Y);
+        var radLat2 = this.radians(Degree2.X);
+        var radLng2 = this.radians(Degree2.Y);
+        var s = Math.acos(Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radLng1 - radLng2) + Math.sin(radLat1) * Math.sin(radLat2));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s;
+    }
+    // 以一个经纬度为中心计算出四个顶点
+    public static  GetDegreeCoordinates(Degree1, distance) {
+        distance = distance * 1000;
+        var dlng = 2 * Math.asin(Math.sin(distance / (2 * EARTH_RADIUS)) / Math.cos(Degree1.X));
+        dlng = Math.abs(this.degrees(dlng));//一定转换成角度数  原PHP文章这个地方说的不清楚根本不正确 后来lz又查了很多资料终于搞定了
+        var dlat = distance / EARTH_RADIUS;
+        dlat = Math.abs(this.degrees(dlat));//一定转换成角度数
+        console.info("result===============",{
+            lat_min: Degree1.X - dlat,
+            lat_max: Degree1.X + dlat,
+            lng_min: Degree1.Y - dlng,
+            lng_max: Degree1.Y + dlng
+        });
+        //left-top, left-bottom, right-top, right-bottom
+        return {
+            lat_min: Degree1.X - dlat,
+            lat_max: Degree1.X + dlat,
+            lng_min: Degree1.Y - dlng,
+            lng_max: Degree1.Y + dlng
+        };
+    }
+}

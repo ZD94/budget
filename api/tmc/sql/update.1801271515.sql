b31814ecc4ac28d6097046e5e777e8cd7ca77cc4
张栋 <dong.zhang@jingli365.com>
2018-01-27 16:17:37 +0800

    add insert sql

---
+++ api/tmc/sql/update.1801271515.sql
@@ -0,0 +1,5 @@
+
+insert into tmc.tmc_types (id, name, tmc_name, sname, template, services, description, created_at, updated_at) values ('ba680384-beac-49e2-b57b-c8cd470454ef', 'E路通', 'E路通商旅', 'elutong', '{ "userName":"用户名", "key":"密码" }', '[2,3]', '中兴商旅e路通是中兴通讯对外推出的企业商旅服务解决方案。依托中兴通讯自身多年的全球财务管理和商旅管理经验，针对现代企业的差旅管理业务现状和未来发展，基于云平台，提供国内、国际商旅全程专业服务，实现了从差旅计划、订火车/酒店、出差总结、报销、支付、账务处理端到端全业务支撑', NOW(),
+ NOW());
+
+insert into tmc.tmc_types (id, name, tmc_name, sname, template, services, description, created_at, updated_at) values ('5b20503d-fb16-4215-95b6-c98c02e62441', '一九易', '一九易商旅', '19e', '{ "merchantId":"用户名", "key":"密码" }', '[2]', '北京一九易站电子商务有限公司（简称“19e”)成立于2009年，是提供便民服务电子商务平台。19e秉承“让生活更简单”的企业使命，致力于为消费者提供更多、更快捷的便民服务', NOW(), NOW());

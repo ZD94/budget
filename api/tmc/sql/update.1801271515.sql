392fe6d384437595224f10d053b38a6e0979b707
张栋 <dong.zhang@jingli365.com>
2018-02-08 18:17:20 +0800

    add new sql

--- api/tmc/sql/update.1801271515.sql
+++ api/tmc/sql/update.1801271515.sql
@@ -3,3 +3,5 @@ insert into tmc.tmc_types (id, name, tmc_name, sname, template, services, descri
  NOW());
 
 insert into tmc.tmc_types (id, name, tmc_name, sname, template, services, description, created_at, updated_at) values ('5b20503d-fb16-4215-95b6-c98c02e62441', '一九易', '一九易商旅', '19e', '{ "merchantId":"用户名", "key":"密码" }', '[2]', '北京一九易站电子商务有限公司（简称“19e”)成立于2009年，是提供便民服务电子商务平台。19e秉承“让生活更简单”的企业使命，致力于为消费者提供更多、更快捷的便民服务', NOW(), NOW());
+
+insert into tmc.tmc_types (id, name, tmc_name, sname, template, services, description, created_at, updated_at) values ('0f88dc20-ebcd-11e7-886b-01c8696efacb', 'spaghetti', 'spaghetti', 'spaghetti', '{ "merchantId":"用户名", "key":"密码" }', '[1,2,3]', 'spaghetti电子商务国际旅行社有限公司，是一家集航空代理、旅游、酒店、差旅管理、会展、物流等为一体的多元化集团企业。公司总部设在广州，在北京、上海、深圳、成都、武汉、长沙、南昌、东莞、台山、青岛、重庆等地均设有分、子公司。', NOW(), NOW());

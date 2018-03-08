26595e30a4ce6a3f837b82ff1b77d9322b99c92b
hexisen <616082046@qq.com>
2018-03-08 16:52:11 +0800

    for test

--- test/model/budget/prefer/hotel-blacklist.test.ts
+++ test/model/budget/prefer/hotel-blacklist.test.ts
@@ -4,22 +4,26 @@
 'use strict';
 import assert = require("assert");
 import HotelBlackListPrefer = require('../../../../model/budget/prefer/hotel-blacklist');
+let data = require("hotel-data");
 
 describe("hotel-blacklist", async () => {
 
-    const prefer = new HotelBlackListPrefer('hotelBlackListPrefer', {});
+    const prefer = new HotelBlackListPrefer('hotelBlackListPrefer', {
+        score: -1000
+    });
 
     it("用例1 should be ok", async () => {
-       //#todo 完善用例1
-    })
+        //#todo 完善用例1
 
-    it("用例2 should be ok", async()=> {
+    });
+
+    it("用例2 should be ok", async () => {
         //#todo  完善用例2
-    })
+    });
 
     var items = [3, 4, 5, 6];
-    items.forEach( (item) => {
-        it(`用例${item} should be ok`, async() => {
+    items.forEach((item) => {
+        it(`用例${item} should be ok`, async () => {
             //#todo 完善${item}
         })
     })

de83cb0d2cc02f44af9e881c04a3f49bcca4ee06
hexisen <616082046@qq.com>
2018-03-08 17:52:00 +0800

    over one hotel-blacklist

---
+++ test/prefer3.test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Mr.He 
+ * @Date: 2018-03-08 17:31:34 
+ * @Last Modified by:   Mr.He 
+ * @Last Modified time: 2018-03-08 17:31:34 
+ * @content what is the content of this file. */
+
+
+require("ts-node").register({ fast: true });
+
+require('./model/budget/prefer/hotel-blacklist.test.ts');

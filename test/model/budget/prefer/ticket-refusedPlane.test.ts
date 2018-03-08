7f1fdd5cbe62582ed6c808f7856a275ac3cf49d4
yuchanglong <changlong.yu@jingli365.com>
2018-03-08 17:40:26 +0800

    update

--- test/model/budget/prefer/ticket-refusedPlane.test.ts
+++ test/model/budget/prefer/ticket-refusedPlane.test.ts
@@ -4,21 +4,33 @@
 'use strict';
 import assert = require("assert");
 import TicketRefusedPlanePrefer = require('../../../../model/budget/prefer/ticket-refusedPlane');
-// import HotelBlackListPrefer = require('../../../../model/budget/prefer/hotel-blacklist');
-// const trafficData = require("./traffic-data.json");
-const dataLength = 100;
+import { IFinalTicket } from 'model/budget/prefer/interface';
+const trafficData = require("./traffic-data.json");
+const dataLength = 101;
+const planeIndex = [];
+const notPlaneIndex = [];
+const initScore = -5000;
+const fs = require("fs")
 describe("traffic-refusedPlane", async () => {
 
-    const prefer = new TicketRefusedPlanePrefer('ticketRefusedPlanePrefer', {});
+    const prefer = new TicketRefusedPlanePrefer('ticketRefusedPlanePrefer', {score: -initScore});
 
     it("data length should be ok", async () => {
-        // assert.equal(dataLength, trafficData.length)
-        // assert.equal(dataLength, 100)
-       
+        assert.equal(dataLength, trafficData.length);
     })
 
     it("用例2 should be ok", async()=> {
-        
+        let result = await prefer.markScoreProcess(trafficData);
+        await Promise.all(result.map(async (item: IFinalTicket, index: number) => {
+            if(planeIndex.indexOf(index) >= 0){
+                assert.equal(item.score, initScore);
+            }
+            if(notPlaneIndex.indexOf(index) < 0){
+                assert.equal(item.score, initScore);
+            }
+        }));
+        // fs.writeFileSync("./file.json", JSON.stringify(result), 'utf-8');
+        // assert.equal(result)
     })
 
     var items = [3, 4, 5, 6];

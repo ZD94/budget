4245c251110fe4e65c2910114dbb30863807936b
yuchanglong <changlong.yu@jingli365.com>
2018-03-09 13:23:31 +0800

    update

--- test/model/budget/prefer/ticket-transitWaitDurationPrefer.test.ts
+++ test/model/budget/prefer/ticket-transitWaitDurationPrefer.test.ts
@@ -10,29 +10,39 @@ const fs = require("fs")
 
 const baseScore = 5000;
 const maxDuration = 300;
-const minDuration = 60;
+const minDuration = 100;
 const subScore = -100;
-var prefer = null;
+const dataLength = 410;
+
+const initScore = 0;
+// var prefer;
+var result;
 
 describe("ticket-transitWaitDurationPrefer", async () => {
 
-    beforeEach(async () => {
+    before(async () => {   
         await Promise.all(trafficData.map((item: IFinalTicketTest) => {
             delete item.score;
             delete item.reasons;
         })); 
 
-        prefer = new TicketTransitWaitDuration("transitWaitDuration", {
+        var prefer = new TicketTransitWaitDuration("transitWaitDuration", {
             maxDuration,
             minDuration,
             baseScore,
             subScore
         });
+        result = await prefer.markScoreProcess(trafficData);
+        // fs.writeFileSync("./file-duration.json", JSON.stringify(result), 'utf-8');
+    
+    })
+
+
+    it("交通数据长度 should be ok", async () => {
+        assert.equal(result.length, dataLength);
     })
     
-    let result = await prefer.markScoreProcess(trafficData);
-    fs.writeFileSync("./file-duration.json", JSON.stringify(result), 'utf-8');
-
+   
     it("存在中转飞机 should be ok", async () => {
         await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
             if([].indexOf(item.index) >= 0) {
@@ -43,8 +53,8 @@ describe("ticket-transitWaitDurationPrefer", async () => {
 
     it("直达飞机 should be ok should be ok", async()=> {
         await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
-            if([].indexOf(item.index) >= 0) {
-                assert.equal(item.score, 0);
+            if([141, 207].indexOf(item.index) >= 0) {
+                assert.equal(item.score, initScore);
             }
             
         }));
@@ -52,32 +62,33 @@ describe("ticket-transitWaitDurationPrefer", async () => {
 
     it("乘坐火车 should be ok should be ok", async()=> {
         await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
-            if([].indexOf(item.index) >= 0) {
-                assert.equal(item.score, 0);
+            if([105, 136].indexOf(item.index) >= 0) {
+                assert.equal(item.score, initScore);
+            }
+        }));
+    })
+    //index=108, 等待时长为 685min
+    it(`中转等待时长超过最大偏好值${maxDuration}min should be ok should be ok`, async()=> {
+        await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
+            if([108].indexOf(item.index) >= 0) {
+                assert.equal(item.score, 43500);
             }
         }));
     })
 
-    it("中转时长超过最大偏好值300min should be ok should be ok", async()=> {
+    //index=97, 等待时长为 55min
+    it(`中转等待时长小于最小偏好值${minDuration}min should be ok should be ok`, async()=> {
         await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
-            if([].indexOf(item.index) >= 0) {
-                assert.equal(item.score, 0);
+            if([97].indexOf(item.index) >= 0) {
+                assert.equal(item.score, 8500);
             }
         }));
     })
-
-    it("中转时长小于最小偏好值300min should be ok should be ok", async()=> {
+    //index=109, 等待时长为 175min
+    it(`中转等待时长满足偏好值范围${minDuration}-${maxDuration}min should be ok should be ok`, async()=> {
         await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
-            if([].indexOf(item.index) >= 0) {
-                assert.equal(item.score, 0);
-            }
-        }));
-    })
-
-    it("中转时长满足偏好值范围50-300min should be ok should be ok", async()=> {
-        await Promise.all(await result.map((item: IFinalTicketTest, index: number) => {
-            if([].indexOf(item.index) >= 0) {
-                assert.equal(item.score, 0);
+            if([109].indexOf(item.index) >= 0) {
+                assert.equal(item.score, 5000);
             }
         }));
     })

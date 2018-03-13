23260c14500e6602a08de8019ecfbb70cba77db2
张栋 <dong.zhang@jingli365.com>
2018-03-13 16:10:57 +0800

    add Increase error trapping

--- model/budget/prefer/hotel-priceDeviationPunishment.ts
+++ model/budget/prefer/hotel-priceDeviationPunishment.ts
@@ -3,6 +3,8 @@ import {AbstractPrefer} from "./index";
 import {IFinalHotel} from "_types/budget";
 
 let data = require('./hotel-data.json')
+let s = -20000.0;
+let k = 100.0;
 
 class PriceDeviation extends AbstractPrefer<IFinalHotel> {
     private score: number
@@ -61,35 +63,34 @@ class PriceDeviation extends AbstractPrefer<IFinalHotel> {
             p2.push(series[i][parseInt(`${parameter3}`)].price);
         }
 
-        for(let hotel of hotels){
-            let s = -2000.0;
-            let k = 100.0;
+        for (let hotel of hotels) {
+            if (!hotel.reasons) hotel.reasons = [];
+            if (!hotel.score) hotel.score = 0;
             if (hotel.price == 0 || hotel.price == undefined) {
                 hotel.price += s;
                 continue;
             }
 
-            var index;
-                if (hotel.star == 0 || hotel.star == undefined) {
-                    index = 0;
-                } else if (hotel.star == 1 || hotel.star == 2 || hotel.star == 3) {
-                    index = 1;
-                } else {
-                    index = 2;
-                }
+            let index;
+            if (hotel.star == 0 || hotel.star == undefined) index = 0
+            if (hotel.star == 1 || hotel.star == 2 || hotel.star == 3) index = 1
+            if (hotel.star == 4 || hotel.star == 5) index = 2
 
-                let _p = p[index];
-                let _p1 = p1[index];
-                let _p2 = p2[index];
-                if (series[index].length <= 10) {
-                    continue;
-                }
-                if (hotel.price >= _p1 && hotel.price <= _p2) {
-                    continue;
-                }
-
+            let _p = p[index];
+            let _p1 = p1[index];
+            let _p2 = p2[index];
+            if (series[index].length <= 10) {
+                continue;
+            }
+            if (hotel.price >= _p1 && hotel.price <= _p2) {
+                continue;
+            }
+            try {
                 let pn = (s * (1 - 2 / (1 + Math.exp(Math.abs(hotel.price - _p) / k))));
                 hotel.score += parseInt(`${pn}`);
+                hotel.reasons.push(`价格偏差惩罚打分 ${hotel.score}`)
+            }catch (e){
+            }
         }
         return hotels
     }
